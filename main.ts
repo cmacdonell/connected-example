97a4fd3162336bc7c9df5642d25f216db8ed0bdd
Cam Macdonell <macdonellc4@macewan.ca>
2021-03-04 14:53:16 -0700

    Detect collision with sprite

--- main.ts
+++ main.ts
@@ -2,25 +2,50 @@ function randomRain () {
     list = [setRain(), setRain(), setRain(), setRain(), setRain()]
     row = 0
 }
+input.onButtonPressed(Button.A, function () {
+    led.unplot(man_x, 4)
+    if (man_x >= 1) {
+        man_x += -1
+    }
+    led.plot(man_x, 4)
+})
+input.onButtonPressed(Button.B, function () {
+    led.unplot(man_x, 4)
+    if (man_x <= 3) {
+        man_x += 1
+    }
+    led.plot(man_x, 4)
+})
 function setRain () {
     return randint(0, 1)
 }
 let row = 0
 let list: number[] = []
+let man_x = 0
 randomRain()
+man_x = 2
+led.plot(man_x, 4)
+let countdown = 500
 basic.forever(function () {
     for (let index = 0; index <= 4; index++) {
         if (list[index]) {
             led.plot(index, row)
         }
     }
-    basic.pause(1000)
+    basic.pause(countdown)
     for (let index = 0; index <= 4; index++) {
         led.unplot(index, row)
     }
     row += 1
     if (row == 4) {
-        row = 0
-        randomRain()
+        if (list[man_x] == 1) {
+            basic.showString("Game Over!")
+        } else {
+            row = 0
+            randomRain()
+            if (countdown > 100) {
+                countdown += -25
+            }
+        }
     }
 })
